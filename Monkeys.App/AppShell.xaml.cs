5a4f67bf15135f0ba56842df6044376a88bfdae9
Shane <shanemonck11@hotmail.com>
2023-04-01 12:02:04 +0800

    Initial Commit

---
+++ Monkeys.App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Monkeys.App;
+
+public partial class AppShell: Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Monkeys.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Monkeys.App;
+
+[Register("AppDelegate")]
+public class AppDelegate: MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
